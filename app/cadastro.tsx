ada6d654d25032a6f432d59fa84a866d425c498d
Victor Hugo <victor.aguilar@etec.sp.gov.br>
2024-09-10 14:37:56 -0300

    App em desenvolvimento - React Native

---
+++ app/cadastro.tsx
@@ -0,0 +1,83 @@
+import { Text, View, TextInput, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { useFonts } from 'expo-font';
+
+export default function Cadastro () {
+    return (
+        <View style = {Styles.container}>
+            <View style = {Styles.form}>
+                <Text style = {Styles.titulo}>Cadastre-se</Text>
+                <Text style = {Styles.label}>Nome Completo</Text>
+                <TextInput style = {Styles.input}/>
+                <Text style = {Styles.label}>Email</Text>
+                <TextInput style = {Styles.input} keyboardType="email-address" />
+                <Text style = {Styles.label}>Senha</Text>
+                <TextInput style = {Styles.input} maxLength={10} secureTextEntry={true}/>
+                <Text style = {Styles.label}>Confirme sua senha</Text>
+                <TextInput style = {Styles.input} maxLength={10} secureTextEntry={true}/>
+                <TouchableOpacity style={[Styles.botao]}>
+                    <Text style = {Styles.botaoTexto}>Criar Conta</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const Styles = StyleSheet.create ({
+    container: {
+        width:"100%",
+        height:"100%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    form: {
+        width: 290,
+        height: 642,
+        display:"flex",
+        flexDirection: "column"
+    },
+
+    titulo: {
+        fontFamily:"Medium",
+        fontSize: 36,
+        marginBottom: 40
+
+    },
+
+    label: {
+        fontFamily: "Light",
+        fontSize: 20,
+        marginBottom: 6
+    },
+
+    input: {
+        fontSize: 16,
+        fontFamily: "Light",
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        height: 44,
+        marginBottom: 31,
+    },
+
+    botao: {
+        width: '100%',
+        height: 50,
+        color: "white",
+        backgroundColor: "black",
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius: 10,
+        marginTop: 12,
+        shadowColor:"#000",
+        shadowOffset: {height: 4, width: 0},
+        shadowOpacity: 0.2,
+        elevation: 4,
+    },
+
+    botaoTexto: {
+        fontFamily: "SemiBold",
+        fontSize: 24,
+        color:"white",
+    }
+
+});
---
+++ app/index.tsx
@@ -0,0 +1,103 @@
+import { Text, View, TextInput, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { useFonts } from 'expo-font';
+import { useRouter } from 'expo-router';
+
+export default function Index() {
+
+  const [fontsLoaded] = useFonts({
+    'Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Light': require('../assets/fonts/Poppins-Light.ttf'),
+    
+  });
+
+  const router = useRouter();
+
+  return (
+    <View style = {Styles.container}>
+      <View style = {Styles.form}>
+        <Text style = {Styles.titulo}>Login</Text>
+        <Text style = {Styles.label}>Email</Text>
+        <TextInput placeholder="Digite seu email" style = {Styles.input} keyboardType="email-address"></TextInput>
+        <Text style = {Styles.label}>Senha</Text>
+        <TextInput placeholder="Digite sua senha" style = {Styles.input} secureTextEntry={true} keyboardType="visible-password"></TextInput>
+        <TouchableOpacity style={[Styles.botao]}>
+          <Text style = {Styles.botaoTexto}>ENTRAR</Text>
+        </TouchableOpacity>
+        <Text style = {Styles.txt}>Não tem conta ainda?</Text>
+        <Text style = {Styles.cadastro} onPress={() => router.push('/cadastro')}>Crie uma conta</Text>
+      </View>
+    </View>
+  );
+}
+
+const Styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
+
+  form: {
+    width: 300,
+    height: 500,
+  },
+
+  titulo: {
+    fontFamily: 'Medium',
+    fontSize: 40,
+    fontWeight: '500',
+    marginBottom: 56,
+  },
+
+  label: {
+    fontFamily:'Light',
+    fontSize: 20,
+    fontWeight:'400',
+    marginBottom: 6,
+  },
+  
+  botao: {
+    width: 300,
+    height: 50,
+    color: "white",
+    backgroundColor: "black",
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius: 10,
+    marginTop: 12,
+    shadowColor:"#000",
+    shadowOffset: {height: 4, width: 0},
+    shadowOpacity: 0.2,
+    elevation: 4,
+  },
+
+  botaoTexto: {
+    fontFamily:'SemiBold',
+    fontSize: 21,
+    color: "white"
+  },
+
+  input: {
+    fontFamily: 'Light',
+    fontSize: 16,
+    height: 44,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 31,
+  },
+
+  txt: {
+    marginTop: 32,
+    fontFamily:'Regular',
+    fontSize: 16,
+  },
+
+  cadastro: {
+    fontSize: 16,
+    fontFamily:'Regular',
+    color: '#0146F6',
+  }
+})
